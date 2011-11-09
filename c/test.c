be99697600862fd71e7bbd3bf0e04685760a5295
Russ Meyerriecks <datachomper@gmail.com>
2011-11-09 16:26:22 -0600

    Seeing some intresting results doing mapping two ways

--- c/test.c
+++ c/test.c
@@ -4,7 +4,6 @@
 
 #define ROWS 200
 #define COLS 200
-#define UNSET -1
 
 enum DIRECTION { n,e,s,w };
 
@@ -13,12 +12,28 @@ struct point {
 	int y;
 };
 
+int loc(int x, int y)
+{
+	return (x*COLS+y);
+}
+
 void print_map(int **map)
 {
 	int r,c;
 	for(r=0; r<ROWS; r++) {
 		for(c=0; c<COLS; c++) {
 			printf("%3d ", map[r][c]);
+		}
+		printf("\n");
+	}
+}
+
+void print_map2(int *map)
+{
+	int r,c;
+	for(r=0; r<ROWS; r++) {
+		for(c=0; c<COLS; c++) {
+			printf("%3d ", map[loc(r,c)]);
 		}
 		printf("\n");
 	}
@@ -32,20 +47,44 @@ struct point neighbor(int row, int col, enum DIRECTION dir)
 	switch(dir) {
 	case n:
 		p.x = (ROWS+row-1)%ROWS;
-		p.y = (COLS+col+0)%COLS;
+		p.y = col;
 		return p;
 	case e:
-		p.x = (ROWS+row+0)%ROWS;
+		p.x = row;
 		p.y = (COLS+col+1)%COLS;
 		return p;
 	case s:
 		p.x = (ROWS+row+1)%ROWS;
-		p.y = (COLS+col+0)%COLS;
+		p.y = col;
 		return p;
 	case w:
-		p.x = (ROWS+row+0)%ROWS;
+		p.x = row;
 		p.y = (COLS+col-1)%COLS;
 		return p;
+	}
+}
+
+int neighbor2(int row, int col, enum DIRECTION dir)
+{
+	int x,y;
+
+	switch(dir) {
+	case n:
+		x = (ROWS+row-1)%ROWS;
+		y = col;
+		return loc(x,y);
+	case e:
+		x = row;
+		y = (COLS+col+1)%COLS;
+		return loc(x,y);
+	case s:
+		x = (ROWS+row+1)%ROWS;
+		y = col;
+		return loc(x,y);
+	case w:
+		x = row;
+		y = (COLS+col-1)%COLS;
+		return loc(x,y);
 	}
 }
 
@@ -77,9 +116,38 @@ void diffuse_iter(int **map)
 	}
 }
 
+void diffuse_iter2(int *map)
+{
+	int r,c,dir,iter,changed;
+	iter = 0;
+	changed = 1;
+	while (changed) {
+		iter++;
+		changed = 0;
+		for (r=0; r<ROWS; r++) {
+			for (c=0; c<COLS; c++) {
+				int lowest = map[loc(r,c)];
+				for (dir=0; dir<4; dir++) {
+					int childval = 0;
+					int tmp;
+					tmp = neighbor2(r, c, dir);
+					childval = map[tmp];
+					if ((childval != 0) && (lowest > childval+1))
+						lowest = childval+1;
+				}
+				if (lowest < map[loc(r,c)]) {
+					changed = 1;
+					map[loc(r,c)] = lowest;
+				}
+			}
+		}
+	}
+}
+
 int main(void)
 {
 	int **map = malloc(ROWS*sizeof(int *));
+	int *map2 = malloc(ROWS*COLS*sizeof(int));
 	int r,c,d,iter;
 	struct point p;
 	struct point neigh;
@@ -89,27 +157,66 @@ int main(void)
 	for(r=0; r<ROWS; r++)
 		map[r] = malloc(COLS*sizeof(int));	
 
+	/* Benchmarking */
 	start = clock();
 	for(iter=0; iter < 100; iter++) {
 		for(r=0; r<ROWS; r++)
 			for(c=0; c<COLS; c++)
 				map[r][c] = 255;
 
-//		print_map(map);
-
 		map[0][0] = 1;
-		map[1][1] = 0;
-		map[1][2] = 0;
-		map[1][3] = 0;
-		map[2][1] = 0;
-		map[3][1] = 0;
 		diffuse_iter(map);
-//		print_map(map);
 	}
 	end = clock();
+//	print_map(map);
 	elapsed = ((double)(end-start))/CLOCKS_PER_SEC;
-	printf("%fs per diffusion\n", elapsed/100);
+	printf("%fs per diffusion\n\n", elapsed/100);
 
+
+	start = clock();
+	for(iter=0; iter < 100; iter++) {
+		for(r=0; r<ROWS; r++)
+			for(c=0; c<COLS; c++)
+				map2[loc(r,c)] = 255;
+
+		map2[loc(0,0)] = 1;
+		diffuse_iter2(map2);
+	}
+	end = clock();
+//	print_map2(map2);
+	elapsed = ((double)(end-start))/CLOCKS_PER_SEC;
+	printf("%fs per diffusion2\n", elapsed/100);
+
+	/* Visualize */
+#if 0
+	for(r=0; r<ROWS; r++)
+		for(c=0; c<COLS; c++)
+			map[r][c] = 255;
+	map[2][2] = 1;
+	map[17][17] = 1;
+	map[0][10] = 0;
+	map[1][10] = 0;
+	map[2][10] = 0;
+	map[3][10] = 0;
+	map[4][10] = 0;
+	map[5][10] = 0;
+	map[6][10] = 0;
+	map[7][10] = 0;
+	map[8][10] = 0;
+	print_map(map);
+	diffuse_iter(map);
+	print_map(map);
+
+	/* Visualize 2 */
+	for(r=0; r<ROWS; r++)
+		for(c=0; c<COLS; c++)
+			map2[loc(r,c)] = 255;
+	print_map2(map2);
+	map2[loc(0,0)] = 1;
+	diffuse_iter2(map2);
+	printf("\n");
+	print_map2(map2);
+#endif
 
 
 
@@ -117,5 +224,6 @@ int main(void)
 	for(r=0; r<ROWS; r++)
 		free(map[r]);
 	free(map);
+	free(map2);
 	return 0;
 }
