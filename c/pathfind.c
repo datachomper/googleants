cd43ea29cde3781e154dbe316ed8fa9d0ebf7c24
datachomper <datachomper@gmail.com>
2011-11-17 01:27:13 -0600

    The start of the astar using kernel lists

--- c/pathfind.c
+++ c/pathfind.c
@@ -1,41 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "types.h"
+#include "list.h"
 
 /* Usage: ./pathfind < mapfile.map */
 
 int ROWS, COLS;
 enum DIRECTION { N,E,S,W };
 
+struct square {
+	struct list_head node;
+	struct square *parent;
+	char x;
+	char y;
+	int f;
+};
+
 int loc(int x, int y)
 {
 	return (x*COLS+y);
 }
 
-int neighbor(int row, int col, enum DIRECTION dir)
+struct list_head *neighbors(char *map, struct square *parent)
 {
-	switch(dir) {
-	case N:
-		if (row == 0)
-			return loc(ROWS-1, col);
-		else
-			return loc(row-1, col);
-	case E:
-		if (col == COLS-1)
-			return loc(row, 0);
-		else
-			return loc(row, col+1);
-	case S:
-		if (row == ROWS-1)
-			return loc(0, col);
-		else
-			return loc(row+1, col);
-	case W:
-		if (col == 0)
-			return loc(row, COLS-1);
-		else
-			return loc(row, col-1);
+	LIST_HEAD(adj);
+	char row, col;
+	enum DIRECTION d;
+
+	for (d=0; d<4; d++) {
+		row = parent->row;
+		col = parent->col;
+		switch(d) {
+		case N:
+			if (row == 0)
+				row = ROWS-1;
+			else
+				row = row-1;
+			break;
+		case E:
+			if (col == COLS-1)
+				col = 0;
+			else
+				col = col+1;
+			break;
+		case S:
+			if (row == ROWS-1)
+				row = 0;
+			else
+				row = row+1;
+			break;
+		case W:
+			if (col == 0)
+				col = COLS-1;
+			else
+				col = col-1;
+			break;
+		}
+		if (map[loc(row,col)] != '%') {
+			struct *square = malloc(sizeof(struct square));
+			square->x = row;
+			square->y = col;
+			&square->parent = parent;
+			list_add(&square->node, &adj);
+		}
 	}
-	return 0;
+	return adj;
 }
 
 void print_map(char *map)
@@ -84,17 +113,37 @@ char * import_map() {
 
 void astar(char *map, int startx, int starty, int endx, int endy)
 {
+	LIST_HEAD(open);
+	LIST_HEAD(closed);
+	struct square *start = malloc(sizeof(struct square));
+	struct square *square;
+	int d;
 
+	start->x = startx;
+	start->y = starty;
+	start->parent = NULL;
+	list_add(&start->node, &open);
+	while (!list_empty(&open)) {
+		list_for_each_entry(square, &open, node) {
+			// Find lowest F value
+		}
+		// Add all valid neighbor moves onto the open list
+		neighbors = neighbors(map, square);
+		list_splice(&neighbors, &open);
+		// Move current square from open to closed
+		list_move(&square->node, &closed);
+	}
 }
 
 int main()
 {
 	char *map;
 
-	map = import_map();
-	if (!map)
-		return -1;
-	print_map(map);
+//	map = import_map();
+//	if (!map)
+//		return -1;
+//	print_map(map);
+	astar(map, 12, 13, 44, 55);
 
 	return 0;
 }
---
+++ c/types.h
@@ -0,0 +1,37 @@
+#ifndef _TYPES_H_
+#define _TYPES_H_
+
+#define LIST_POISON1 ((void *)0x10101010)
+#define LIST_POISON2 ((void *)0x20202020)
+
+struct list_head {
+        struct list_head *next, *prev;
+};
+
+struct hlist_head {
+        struct hlist_node *first;
+};
+
+struct hlist_node {
+        struct hlist_node *next, **pprev;
+};
+
+#undef offsetof
+#ifdef __compiler_offsetof
+#define offsetof(TYPE,MEMBER) __compiler_offsetof(TYPE,MEMBER)
+#else
+#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
+#endif
+
+/**
+ * container_of - cast a member of a structure out to the containing structure
+ * @ptr:        the pointer to the member.
+ * @type:       the type of the container struct this is embedded in.
+ * @member:     the name of the member within the struct.
+ *
+ */
+#define container_of(ptr, type, member) ({                      \
+        const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
+        (type *)( (char *)__mptr - offsetof(type,member) );})
+
+#endif
