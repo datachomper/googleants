0ec30568199340c25d1fb259c1f85134a5abca39
Russ Meyerriecks <datachomper@gmail.com>
2011-11-24 18:12:51 -0600

    Added a game start script

---
+++ c/play.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+ENGINE="/home/russ/projects/googleants/tools/playgame.py"
+MYBOT="/home/russ/projects/googleants/c/MyBot"
+OTHERBOT="python /home/russ/projects/googleants/tools/sample_bots/python/HunterBot.py"
+#MAP="/home/russ/projects/googleants/tools/maps/example/tutorial1.map"
+MAP="/home/russ/projects/googleants/tools/maps/maze/maze_02p_02.map"
+
+${ENGINE} --log_dir log --log_input --turns 500 --player_seed 42 --verbose -E --fill --map_file ${MAP} ${MYBOT} "${OTHERBOT}"
+
+#python ../tools/playgame.py "./MyBot" "python ../tools/sample_bots/python/HunterBot.py" --map_file ../tools/maps/example/tutorial1.map --log_dir log --log_input --turns 60 --scenario --food none --player_seed 7 --verbose -E --end_wait 0.25
+#cat ./log/0.bot0.error
